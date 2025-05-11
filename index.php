291a56eb34274afa2221517417aa0728c6099f41
root <root@ip-172-31-25-39.ec2.internal>
2025-05-11 08:00:26 +0000

    sending to nginx

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-echo "Hello from PHP on EC2 with CI/CD!\nI am adding another line for this \n";
+echo "Hello from PHP on EC2 with CI/CD!\nI am adding another line for this \nfinallly sending to webpage";
 ?>
